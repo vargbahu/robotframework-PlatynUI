f601db7b27344347285b08a8084efdc859fb168b
Daniel Biehl <daniel.biehl@imbus.de>
2024-07-22 05:24:30 +0200

    feat: introduce Semi.Avalonia theme for spy

---
+++ src/PlatynUI.Platform.Win32/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2024 Daniel Biehl <daniel.biehl@imbus.de>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
