5c604867abe107e71c5bdcad8ed1272fea4691a2
Daniel Biehl <daniel.biehl@imbus.de>
2024-07-26 18:16:10 +0200

    fix(Spy): some correction theming for different platforms

--- tests/apps/AvaloniaTestApp/ViewModels/MainWindowViewModel.cs
+++ tests/apps/AvaloniaTestApp/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,19 @@
-﻿namespace AvaloniaTestApp.ViewModels;
+﻿using System;
+using ReactiveUI;
+
+namespace AvaloniaTestApp.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    private string _greeting = "Welcome to Avalonia!";
+    public string Greeting
+    {
+        get => _greeting;
+        set
+        {
+            if (value.Contains("hi"))
+                throw new ArgumentException("No hi allowed");
+            this.RaiseAndSetIfChanged(ref _greeting, value);
+        }
+    }
 }
